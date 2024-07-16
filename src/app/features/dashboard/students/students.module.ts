1adb51262eff5d8002a4a7f617191816a3280b1e
Lean Ale <leandroalegre@gmail.com>
2024-07-16 10:20:00 -0300

    Correccion scss

--- src/app/features/dashboard/students/students.module.ts
+++ src/app/features/dashboard/students/students.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatTableModule } from '@angular/material/table';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-
+import { SharedModule } from 'src/app/shared/shared.module';
 import { StudentsComponent } from './students.component';
 import { StudentsDialogComponent } from './component/students-dialog/students-dialog.component';
 import { FullNamePipe } from './pipes/full-name.pipe';
@@ -28,7 +28,8 @@ import { TitleSizeDirective } from './directives/title-size.directive';
     MatTableModule,
     MatDialogModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    SharedModule
   ],
   exports: [
     StudentsComponent
