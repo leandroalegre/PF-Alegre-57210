0b7807f32fd7a526fcd12b2c2fbc94d09edc50da
Leandro Alegre <leandroalegre@gmail.com>
2024-08-06 23:27:47 -0300

    LazyCorrect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './features/auth/login/login.component';
 import { DashboardComponent } from './features/dashboard/dashboard.component';
-import { CoursesComponent } from './features/dashboard/courses/courses.component';
-import { StudentsComponent } from './features/dashboard/students/students.component';
-import { TeachersComponent } from './features/dashboard/teachers/teachers.component';
-import { CoursesDetailsComponent } from './features/dashboard/courses/pages/courses-details/courses-details.component';
-import { HomeComponent } from './features/dashboard/home/home.component';
 import { authGuard } from './core/guards/auth.guard';
 
 
